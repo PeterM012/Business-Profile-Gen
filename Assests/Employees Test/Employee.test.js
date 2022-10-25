f09c67ba264b71c68d18c3dfb5b0b192f9488be6
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-24 19:02:52 -0700

    add updated Employee test js

--- Assests/Employees Test/Employee.test.js
+++ Assests/Employees Test/Employee.test.js
@@ -1,8 +1,24 @@
 const Employee = require('../Employees/Employee');
 const employee = new Employee('newPerson','newID','newEmail');
 
-test('test constructors'), () => {
+test('test constructors', () => {
     expect(employee.name).toBe('newPerson');
-    expect(employee.id).toBe('newId')
+    expect(employee.id).toBe('newId');
     expect(employee.name).toBe('newEmail');
-}
+});
+
+test('test Name()', () => {
+    expect(employee.getName()).toBe('newPerson');
+});
+
+test('test getId()', () => {
+    expect(employee.getId()).toBe('newId');
+});
+
+test('test getEmail()', () => {
+    expect(employee.getEmail()).toBe('newEmail');
+});
+
+test('test getRole()', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
